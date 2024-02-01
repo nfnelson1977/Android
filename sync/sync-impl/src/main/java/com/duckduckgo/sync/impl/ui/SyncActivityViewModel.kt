802676219054fdf093c5dd20a8e9b3679ec4af82
Cristian Monforte <cristian@duckduckgo.com>
2024-02-01 23:02:03 +0100

    Encryption not supported screen

--- sync/sync-impl/src/main/java/com/duckduckgo/sync/impl/ui/SyncActivityViewModel.kt
+++ sync/sync-impl/src/main/java/com/duckduckgo/sync/impl/ui/SyncActivityViewModel.kt
@@ -47,6 +47,7 @@ import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.AskTurnOffSync
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.CheckIfUserHasStoragePermission
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.IntroCreateAccount
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.RecoveryCodePDFSuccess
+import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.ShowDeviceUnsupported
 import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.ShowError
 import com.duckduckgo.sync.impl.ui.SyncDeviceListItem.LoadingItem
 import com.duckduckgo.sync.impl.ui.SyncDeviceListItem.SyncedDevice
@@ -79,7 +80,9 @@ class SyncActivityViewModel @Inject constructor(
 
     private val command = Channel<Command>(1, DROP_OLDEST)
     private val viewState = MutableStateFlow(ViewState())
-    fun commands(): Flow<Command> = command.receiveAsFlow()
+    fun commands(): Flow<Command> = command.receiveAsFlow().onStart {
+        checkIfDeviceSupported()
+    }
 
     fun viewState(): Flow<ViewState> =
         viewState.onStart {
@@ -100,6 +103,15 @@ class SyncActivityViewModel @Inject constructor(
             syncEngine.triggerSync(FEATURE_READ)
         }.flowOn(dispatchers.io())
             .launchIn(viewModelScope)
+    }
+
+    private suspend fun checkIfDeviceSupported() {
+        val isSupported = withContext(dispatchers.io()) {
+            syncAccountRepository.isSyncSupported()
+        }
+        if (!isSupported) {
+            command.send(ShowDeviceUnsupported)
+        }
     }
 
     private suspend fun signedInState(): ViewState {
@@ -160,6 +172,7 @@ class SyncActivityViewModel @Inject constructor(
         data class AskRemoveDevice(val device: ConnectedDevice) : Command()
         data class AskEditDevice(val device: ConnectedDevice) : Command()
         data class ShowError(@StringRes val message: Int, val reason: String = "") : Command()
+        object ShowDeviceUnsupported : Command()
     }
 
     fun onSyncWithAnotherDevice() {
